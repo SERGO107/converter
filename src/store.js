aadf8d92c1a67a54b73502fc616d1741e522d854
SERGO107 <sergserg10747@gmail.com>
2021-06-01 19:54:42 +0300

    clear

--- src/store.js
+++ src/store.js
@@ -3,33 +3,27 @@ import { createContext } from 'react'
 
 class Store {
     view = 1
-    input = 'random input'
-    pre = 'you input:'
-    country = ''
     curencyData = []
 
-    constructor() {
-        //со значением автообсервбл не нужно расставлять декораторы (@action, @observeble, @computed,@reaction) теперь mobx распознает значения автоматически
+    constructor() {       
         makeAutoObservable(this)
     }
+
     handleSubmit = (incomeValue) => {
         console.log(incomeValue)
         parseInt(incomeValue)
         this.view = incomeValue
     }
 
-
-
     async fetchCurency() {
         const response = await fetch(`https://www.nbrb.by/api/exrates/rates?periodicity=0`)
         const data = await response.json()
         runInAction(() => {
-            this.curencyData = data
-            console.log(data)
+            this.curencyData = data            
         })
     }
 
 
 }
-//чтобы испоьзовать данные из стора в реакт компонентах нужно обернуть стор в реакт контекст
+
 export default createContext(new Store())
